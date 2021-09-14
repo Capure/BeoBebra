b7d308fbf4d9d67be93fe769f7114fedb8743371
Kacper Stolarek <stolarek83@gmail.com>
2021-09-14 20:57:52 +0200

    :tada: Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { CanvasHolder } from "./components/CanvasHolder";
 
 function App() {
+  const [canvasController, setCanvasController] = useState<any>();
+
+  const draw = (canvasController: any) => {
+    canvasController.drawLine(10, 10, 100, 100, "#0f025c");
+  };
+
+  const setup = (canvasController: any) => {
+    setCanvasController(canvasController);
+    draw(canvasController);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CanvasHolder onCanvasReady={setup} />
+    </>
   );
 }
 
